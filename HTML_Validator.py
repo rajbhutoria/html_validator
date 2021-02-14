f5ad1cd3274ddeb72f5aa39ff3a060d364bbab19
rajbhutoria <rbhutoria22@cmc.edu>
2021-02-14 14:44:22 -0800

    Fix html_validator and _extract_tags

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -1,6 +1,5 @@
 #!/bin/python3
 
-
 def validate_html(html):
     '''
     This function performs a limited version of html validation by checking whether every opening tag has a corresponding closing tag.
@@ -10,7 +9,27 @@ def validate_html(html):
     >>> validate_html('<strong>example')
     False
     '''
-
+    tags = _extract_tags(html)
+    openTags = []
+    if html  == '':
+        return True
+    elif tags == []:
+        return False
+    else:
+        for tag in tags:
+            if '/' not in tag:
+                openTags.append(tag)
+            else:
+                if len(openTags) == 0:
+                    return False
+                strippedCloseTag = tag[2:-1]
+                strippedOpenTag = openTags[-1][1:-1]
+                if strippedOpenTag == strippedCloseTag:
+                    openTags.pop()
+        if len(openTags) == 0:
+            return True
+        else:
+            return False 
     # HINT:
     # use the _extract_tags function below to generate a list of html tags without any extra text;
     # then process these html tags using the balanced parentheses algorithm from the class/book
@@ -20,7 +39,7 @@ def validate_html(html):
 
 def _extract_tags(html):
     '''
-    This is a helper function for `validate_html`.
+    This is a helper function for validate_html.
     By convention in Python, helper functions that are not meant to be used directly by the user are prefixed with an underscore.
 
     This function returns a list of all the html tags contained in the input string,
@@ -29,3 +48,19 @@ def _extract_tags(html):
     >>> _extract_tags('Python <strong>rocks</strong>!')
     ['<strong>', '</strong>']
     '''
+    final_list = []
+    tags = ''
+    X = False
+    for i in html:
+        if i == '<':
+            X = True
+            tags += i
+        elif i == '>':
+            X = False
+            tags += i
+            final_list += [tags]
+            tags = ''
+        else:
+            if X == True:
+                tags += i
+    return final_list
